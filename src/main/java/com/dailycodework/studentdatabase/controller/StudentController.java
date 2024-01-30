7bd5e7e972748bb0df3b6194c0ebc71a40f56e9d
Nola360 <Adaramolajr@gmail.com>
2024-01-30 02:13:18 -0500

    added cross origin annotation to controller class

--- src/main/java/com/dailycodework/studentdatabase/controller/StudentController.java
+++ src/main/java/com/dailycodework/studentdatabase/controller/StudentController.java
@@ -12,7 +12,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/students")
 @RequiredArgsConstructor
-
+@CrossOrigin(origins = "http://localhost:4200")
 public class StudentController {
 
     private final IStudentService studentService;
